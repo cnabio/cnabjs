1fbb9d4bfd64a8146093d6af65d296d2ba61e005
Ivan Towlson <itowlson@microsoft.com>
2019-08-01 15:04:31 +1200

    Plumbing for unit tests

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+import assert from 'assert';
+
+describe('mathematics', () => {
+    it('should be consistent', () => {
+        assert.equal(1 + 1, 2, "call Bertrand Russell pronto");
+    });
+});
